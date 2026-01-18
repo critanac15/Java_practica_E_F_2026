9c8ef0503a7d0270a9fa8ad73c8641a80502770e
critanac15 <cristiann0815@gmail.com>
2026-01-18 13:57:20 -0500

    actualizacion de leer cadena

--- src/ejemplo3/leercadena.java
+++ src/ejemplo3/leercadena.java
@@ -1,4 +1,8 @@
 package ejemplo3;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class leercadena {
+    private static BufferedReader dato=new BufferedReader(new InputStreamReader(System.in));
 }
