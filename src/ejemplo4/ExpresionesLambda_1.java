6e8dd2c2f76d95b64bcb2b7addbf6955df2657b8
critanac15 <cristiann0815@gmail.com>
2026-01-25 08:39:48 -0500

    Expresiones lambda

---
+++ src/ejemplo4/ExpresionesLambda_1.java
@@ -0,0 +1,12 @@
+package ejemplo4;
+
+public class ExpresionesLambda_1 {
+    public static void main(String[] args) {
+
+        //Definir la expresion lambda y guardarla en una variable de tipo runnable
+        Runnable eje=() -> System.out.println(("Hola mundo"));
+
+        //Llamar a la variable para que se ejecute
+        eje.run();
+    }
+}
